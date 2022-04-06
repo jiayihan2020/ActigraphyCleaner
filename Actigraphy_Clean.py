77b8f49e7b97ae2bd7222934a47493db3c27815d
Han Jia Yi <pandaboy94+signup@gmail.com>
2022-04-06 17:14:56 +0800

    Added functionality to filter data

---
+++ Actigraphy_Clean.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import re
+import csv
+import os
+
+directory = "SIT_controlGroup/SIT011.csv"
+
+
+def actigraphy_data():
+    target = re.compile(r"-------------------- Epoch-by-Epoch Data -------------------")
+    row = 17
+
+    with open(directory) as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for item in csv_reader:
+            if not re.search(target, str(item)):
+                row += 1
+            else:
+                break
+    df = pd.read_csv(
+        directory,
+        skiprows=row,
+    )
+    df = df.filter(["Line", "Date", "Time", "Activity"])
+    df = df.rename(columns={"Activity": "Axis"})
+    try:
+        df.to_csv(
+            "/SIT_ControlGroupFiltered/SIT001_allepoch.csv",
+            index=False,
+            encoding="utf-8",
+        )
+    except OSError:
+        os.mkdir("SIT_ControlGroupFiltered")
+        df.to_csv(
+            "./SIT_ControlGroupFiltered/SIT001_allepoch.csv",
+            index=False,
+            encoding="utf-8",
+        )
+
+    return df.head(20)
+
+
+actigraphy_data()
