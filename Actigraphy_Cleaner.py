22835196ac5b3c06a1ca73ffcd56b4d5c2b92ff1
Han Jia Yi <jiayi842000@yahoo.com.sg>
2022-05-24 23:14:00 +0800

    Attempt to fix incorrect parsing of datetime

--- Actigraphy_Cleaner.py
+++ Actigraphy_Cleaner.py
@@ -1,10 +1,11 @@
+from genericpath import isdir
 import pandas as pd
 import re
 import csv
 import os
 
-input_directory = "SIT_controlGroup"
-output_directory = "SIT_control Group"
+input_directory = "SIT_LTLB"
+output_directory = "SIT_LTLB Group"
 os.chdir(input_directory)
 
 
@@ -15,49 +16,49 @@ def actigraphy_data():
         print(f"Now reading {file}...")
 
         row = 17
+        if not isdir(file):
 
-        with open(file) as csv_file:
-            csv_reader = csv.reader(csv_file)
+            with open(file) as csv_file:
+                csv_reader = csv.reader(csv_file)
 
-            for item in csv_reader:
-                if not re.search(target, str(item)):
-                    row += 1
+                for item in csv_reader:
+                    if not re.search(target, str(item)):
+                        row += 1
+                    else:
+                        break
+            while True:
+                df = pd.read_csv(file, skiprows=row)
+
+                try:
+                    df.iloc[0]["Line"]
+                except KeyError:
+                    row -= 1
                 else:
                     break
-        while True:
-            df = pd.read_csv(
-                file,
-                skiprows=row,
-            )
+            df = df.filter(["Date", "Time", "Activity"])
+            df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y")
+            df = df.rename(columns={"Activity": "Axis1"})
+            df["Axis1"].fillna(0, inplace=True)
+            df["Date"] = pd.to_datetime(df["Date"]).dt.strftime(
+                "%-d/%-m/%y"
+            )  # For Windows, try replacing '-' with '#'
+            # df["Time"].apply(pd.to_datetime)
+            # df["Time"] = pd.to_datetime(df["Time"]).dt.strftime("%#H:%M")
+            output_filename = file.split(".")[0]
 
             try:
-                df.iloc[0]["Line"]
-            except KeyError:
-                row -= 1
-            else:
-                break
-        df = df.filter(["Date", "Time", "Activity"])
-        df = df.rename(columns={"Activity": "Axis1"})
-        df["Axis1"].fillna(0, inplace=True)
-        df["Date"].apply(pd.to_datetime, infer_datetime_format=True)
-        df["Date"] = pd.to_datetime(df["Date"]).dt.strftime("%#d/%#m/%Y")
-        df["Time"].apply(pd.to_datetime)
-        df["Time"] = pd.to_datetime(df["Time"]).dt.strftime("%#H:%M")
-        output_filename = file.split(".")[0]
-
-        try:
-            df.to_csv(
-                f"./{output_directory}/{output_filename}_all epoch.csv",
-                index=False,
-                encoding="utf-8",
-            )
-        except OSError:
-            os.mkdir(f"{output_directory}")
-            df.to_csv(
-                f"./{output_directory}/{output_filename}_all epoch.csv",
-                index=False,
-                encoding="utf-8",
-            )
+                df.to_csv(
+                    f"./{output_directory}/{output_filename}_all epoch.csv",
+                    index=False,
+                    encoding="utf-8",
+                )
+            except OSError:
+                os.mkdir(f"{output_directory}")
+                df.to_csv(
+                    f"./{output_directory}/{output_filename}_all epoch.csv",
+                    index=False,
+                    encoding="utf-8",
+                )
 
     return None
 
